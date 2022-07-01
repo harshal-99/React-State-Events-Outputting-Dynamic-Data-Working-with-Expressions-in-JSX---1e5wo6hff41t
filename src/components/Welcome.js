db13a8f65c08e4dac6d88fe1bfe550c4eb97cfcc
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2022-07-01 21:19:48 +0530

    .

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,8 @@
-// write code for Welcome component here
+const Welcome = ({name}) => {
+  return <div>
+    <h1>Hey ! {name}</h1>
+    <h2>Welcome to Newton School.</h2>
+  </div>
+}
+
+export default Welcome
